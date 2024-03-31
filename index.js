dbe2770fadf860a82aff8e9c063ac67ec2cf7fca
Hamza-Yaseen1 <hamza.sce1@gmail.com>
2024-03-31 21:38:10 +0500

    Simple Calculator using TS

---
+++ index.js
@@ -0,0 +1,12 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter Second number", type: "number", name: "secondNumber" },
+    {
+        message: "Select operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+console.log(answer);
